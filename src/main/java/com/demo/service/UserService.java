ffe43ab150666ce26ed19d15c717f32f0fd32ce8
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-15 21:43:03 +0530

    logout and login and add methodwas added on the UserService class

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -1,5 +1,53 @@
 package com.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.user.User;
+
+@Service
 public class UserService {
 
+	
+	public final List<User>userlist =new ArrayList<>();
+	
+	public void add(User user)
+	{
+	user.setStatus("online");
+	userlist.add(user);	
+	}
+	
+	
+	public User login(User user)
+	{
+		for(int j=0;j<userlist.size();j++)
+		{
+			if(userlist.get(j).getEmail().equals(user.getEmail()))
+			{
+				return user;
+			}
+		}
+		return null;
+		
+		
+		
+	}
+	
+	public void logout(String email)
+	{
+		for(int j=0;j<userlist.size();j++)
+		{
+			if(userlist.get(j).getEmail().equals(email))
+			{
+				userlist.get(j).setStatus("offline");
+			}
+		}
+	
+		
+	}
+	
+	
+	
 }
