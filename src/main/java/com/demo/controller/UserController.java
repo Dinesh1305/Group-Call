075e8cab5f1d9a27c0b29a60141a8e9a1a9a8bad
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-15 22:24:18 +0530

    pom.xml was updated and login page was created

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -3,22 +3,29 @@ package com.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.demo.service.UserService;
 import com.demo.user.User;
 
 import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
-@RestController
-@CrossOrigin(origins="*")
+@Controller
 
 public class UserController {
 	@Autowired
 	private UserService service;
+	
+	
+	@RequestMapping("/")
+	public String index()
+	{
+		return "login";
+	}
 	
 	
 	@PostMapping("login")
@@ -47,4 +54,6 @@ public class UserController {
 	}
 	
 	
+	
+
 }
